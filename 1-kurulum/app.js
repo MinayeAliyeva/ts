ca0280a9bf637d13c4b2bff3ed3108eafc97c4b4
MinayeAliyeva <7s346pu@code.edu.az>
2023-11-09 14:41:50 +0400

    functions added

--- 1-kurulum/app.js
+++ 1-kurulum/app.js
@@ -1,14 +1,24 @@
-// function add(a:number,b:number){
-//   return a+b;
-// }
-// let sum=add(3,5)
-// console.log(sum);
-// function bastir(): void {
-//   console.log("Can Boz");
-// }
-// bastir();
+function add(a, b) {
+    return a + b;
+}
+var sum = add(3, 5);
+console.log(sum);
+function bastir() {
+    console.log("Can Boz");
+}
+bastir();
 function birlesdir(name, sname) {
     if (sname === void 0) { sname = "Boz"; }
     return "".concat(name, "  ").concat(sname);
 }
 console.log(birlesdir('Minaya'));
+function carpim(a, b, c) {
+    if (typeof c !== "undefined") {
+        return a * b * c;
+    }
+    return a * b;
+}
+console.log(carpim(3, 4, 5));
+//arrow
+var carpim = function () { return console.log('jj'); };
+carpim();
--- 1-kurulum/app.ts
+++ 1-kurulum/app.ts
@@ -1,14 +1,14 @@
-// function add(a:number,b:number){
-//   return a+b;
+function add(a:number,b:number){
+  return a+b;
 
-// }
-// let sum=add(3,5)
-// console.log(sum);
+}
+let sum=add(3,5)
+console.log(sum);
 
-// function bastir(): void {
-//   console.log("Can Boz");
-// }
-// bastir();
+function bastir(): void {
+  console.log("Can Boz");
+}
+bastir();
 
 function birlesdir(name:string,sname:string="Boz"):string{
  return `${name}  ${sname}`
@@ -16,3 +16,17 @@ function birlesdir(name:string,sname:string="Boz"):string{
 }
 console.log(birlesdir('Minaya'));
 
+function carpim(a: number, b: number, c?: number) {
+  if (typeof c !== "undefined") {
+    return a * b * c;
+  }
+  return a * b;
+}
+
+console.log(carpim(3,4,5));
+
+//arrow
+let carpim=():void=>console.log('jj');
+ 
+
+carpim()
